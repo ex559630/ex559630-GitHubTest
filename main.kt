e4747ad1087d45327231e2d816bef7e788c69b83
ex559630 <ex559630@gmail.com>
2021-06-16 16:15:22 +0800

    V4
    V4
    function and length

--- main.kt
+++ main.kt
@@ -1,7 +1,16 @@
+fun getStringLength(obj:Any):Int?{
+    if (obj is String){
+        return obj.length
+    }
+    return null
+}
 fun main(args: Array<String>) {
+    fun printLength(obj:Any){
+        println("'$obj'你的名字共有${getStringLength(obj)?:"...錯誤，這個不是一個字串"}個字")
+            }
     println("Hello World!")
     println("你好世界")
     println("輸入:")
     val name =readLine()
-    println("輸入為:$name")
+    printLength("$name")
 }
